de7ae4ad2e40ec333b6954557a838770d36b3d25
Julianne Costa <julesleighcosta@gmail.com>
2017-05-08 18:32:03 -0700

    Finish CSS for auto and tabs

--- w6d5/frontend/tabs.jsx
+++ w6d5/frontend/tabs.jsx
@@ -21,9 +21,7 @@ class Tabs extends React.Component {
           this.props.tabs.map((tab) => <li className="tab-titles" onClick={this.handleClick.bind(this)}>{tab.title}</li>)
         }
         </ul>
-        <div className="flx flx-justify tab-content">
-          //how to make this take up the entire content section?
-            //is this issue related to tabs having unwanted backgrounds?
+        <div className="flx tab-content">
           <article>
             {this.props.tabs[this.state.selectedIdx].content}
           </article>
